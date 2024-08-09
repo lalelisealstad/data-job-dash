8b9af6648a8999ab67a41172bc931f00b46ddd51
Elise Alstad <lal@ssb.no>
2024-08-09 18:11:17 +0100

    dash working draft

--- README.md
+++ README.md
@@ -24,3 +24,13 @@ Run the program after installation:
 $ source .venv/bin/activate
 $ python "main.py"
 ```
+
+
+## Develop
+- error message to use if there is no filter
+- 
+- make date range picker nicer
+- update layout
+- add wordcloud and make it match style 
+- add other figures
+- 
--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from dash import dcc, html, Input, Output, State
 import pandas as pd
 import plotly.express as px
 import os
+from collections import Counter
+from datetime import datetime
+from datetime import date
+
+global gdf
+global df
 
 # Initialize the Dash app
 app = dash.Dash(__name__)
@@ -12,19 +18,34 @@ def load_data():
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"../service-account-details.json" 
     file_path_gcp = "gs://oslo-linkedin-dataengineer-jobs/transformed"
     df = pd.read_parquet(file_path_gcp)
-    df = df.replace({'seniority':{None:'Mid / not specified'}})
+    df = df.replace({'seniority': {None: 'Mid / not specified'}})
     return df
+
+gdf = load_data()
 
 # Define the layout of the app
 app.layout = html.Div([
     html.Div([
         html.H2("Filters"),
         html.Label("Filter 1"),
-        dcc.Dropdown(id='filter1', options=[], multi=False),
+        dcc.Dropdown(id='filter1', options=[{'label': 'Data Analyst', 'value': 'Data Analyst'},
+                                            {'label': 'Data Engineer', 'value': 'Data Engineer'},
+                                            {'label': 'Data Scientist', 'value': 'Data Scientist'}],
+                     multi=True, value=[], placeholder="Filter job type", clearable=False),
         html.Label("Filter 2"),
-        dcc.Dropdown(id='filter2', options=[], multi=False),
-        html.Label("Filter 3"),
-        dcc.Dropdown(id='filter3', options=[], multi=False),
+        dcc.Dropdown(id='filter2', options=[{'label': 'Mid / not specified', 'value': 'Mid / not specified'},
+                                            {'label': 'Junior', 'value': 'Junior'},
+                                            {'label': 'Senior', 'value': 'Senior'},
+                                            {'label': 'Lead', 'value': 'Lead'},
+                                            {'label': 'Manager', 'value': 'Manager'}],
+                     multi=True, value=[], placeholder="Filter seniority type", clearable=True),
+         html.Label("Filter 3"),
+        dcc.DatePickerRange(
+            id='filter3',
+            start_date=None,
+            end_date=None,
+            display_format='YYYY-MM-DD',
+        ),
         html.Label("Filter 4"),
         dcc.Dropdown(id='filter4', options=[], multi=False),
     ], style={'width': '20%', 'display': 'inline-block', 'verticalAlign': 'top', 'padding': '20px', 'backgroundColor': '#f4f4f4'}),
@@ -35,51 +56,58 @@ app.layout = html.Div([
     ], style={'width': '75%', 'display': 'inline-block', 'padding': '20px'}),
 ])
 
-# Callback to populate the dropdowns with unique values
+# Callback to populate the date picker and dropdowns with unique values
 @app.callback(
-    [Output('filter1', 'options'),
-     Output('filter2', 'options'),
-     Output('filter3', 'options'),
+    [Output('filter3', 'start_date'),
+     Output('filter3', 'end_date'),
      Output('filter4', 'options')],
-    [Input('filter1', 'id')]  # Dummy input to trigger the callback
+    [Input('filter1', 'value')]
 )
+
 def set_dropdown_options(_):
-    df = load_data()
-    return [
-        [{'label': i, 'value': i} for i in df.job_type.unique().tolist()],
-        [{'label': i, 'value': i}for i in df.seniority.unique().tolist()],
-        [{'label': i, 'value': i}for i in df['date'].dt.date.unique().tolist()],
-        df.explode('skills')['skills'].tolist(),
-    ]
+    if gdf.date.max().date() < date.today():
+        df = load_data()
+    else: 
+        df = gdf
+    
+    min_date = df['date'].min().date()
+    max_date = df['date'].max().date()
+    
+    skill_options = df.explode('skills')['skills'].tolist()
+    
+    return min_date, max_date, skill_options
 
 # Callback to update the graph based on selected filters
-@app.callback(
+@app.callback( 
     Output('graph', 'figure'),
     [Input('filter1', 'value'),
      Input('filter2', 'value'),
-     Input('filter3', 'value'),
+     Input('filter3', 'start_date'),
+     Input('filter3', 'end_date'),
      Input('filter4', 'value')]
 )
-def update_graph(filter1, filter2, filter3, filter4):
-    df = load_data()
-
+def update_graph(filter1, filter2, start_date, end_date, filter4):
+    df = gdf
+    
     # Apply filters
     if filter1:
-        df = df[df['job_type'] == filter1]
+        df = df[df['job_type'].isin(filter1)]
     if filter2:
-        df = df[df['seniority'] == filter2]
-    if filter3:
-        df = df[df['date'] == filter3]
+        df = df[df['seniority'].isin(filter2)]
+    if start_date and end_date:
+        df = df[(df['date'] >= start_date) & (df['date'] <= end_date)]
     if filter4:
+        print(filter4)
         df = df[df['skills'].map(lambda x: filter4 in x)]
+        print(df)
     
-    #### MAKE GRAPH
+    # Define coding languages and cloud skills
     coding_languages = [
-    'python', 'javascript', 'java', 'c++', 'c#', 'php', 'ruby', 'swift', 'kotlin', 'typescript',
-    'go', 'rust', 'sql', 'r', 'html', 'css', 'bash', 'perl', 'objective-c', 'scala',
-    'lua', 'haskell', 'matlab', 'dart', 'visual basic .net', 'assembly language', 'f#', 'groovy', 'elixir', 'clojure',
-    'erlang', 'julia', 'vbscript', 'lisp', 'prolog', 'scheme', 'ada', 'fortran', 'cobol', 'pascal',
-    'racket', 'scratch', 'tcl', 'smalltalk', 'actionscript', 'awk', 'ocaml', 'pl/sql', 'sas', 'logo'
+        'python', 'javascript', 'java', 'c++', 'c#', 'php', 'ruby', 'swift', 'kotlin', 'typescript',
+        'go', 'rust', 'sql', 'r', 'html', 'css', 'bash', 'perl', 'objective-c', 'scala',
+        'lua', 'haskell', 'matlab', 'dart', 'visual basic .net', 'assembly language', 'f#', 'groovy', 'elixir', 'clojure',
+        'erlang', 'julia', 'vbscript', 'lisp', 'prolog', 'scheme', 'ada', 'fortran', 'cobol', 'pascal',
+        'racket', 'scratch', 'tcl', 'smalltalk', 'actionscript', 'awk', 'ocaml', 'pl/sql', 'sas', 'logo'
     ]
     cloud_skills = {
         'aws': 'amazon web services',
@@ -92,31 +120,20 @@ def update_graph(filter1, filter2, filter3, filter4):
         'do': 'digitalocean'
     }
 
-    # share job_type of filtered df
     job_type_share = (df.job_type.value_counts(normalize=True) * 100).astype(int)
-
-    # Explode the 'skills' column and calculate value counts
+    # Explode the 'skills' column and calculate value counts\
     skills_counts = pd.DataFrame(df.explode('skills')['skills'].value_counts())
-
-    # Calculate the total number of skills observations
+    
     total_skills = len(df)
-
-    skills_counts = pd.DataFrame(df.explode('skills')['skills'].value_counts())
-
-    skills_counts['percent'] = (((skills_counts['count'] / total_skills) * 100).astype(int))
+    skills_counts['percent'] = ((skills_counts['count'] / total_skills) * 100).astype(int)
 
     # Filter coding languages and cloud skills
     df_coding_languages = skills_counts[skills_counts.index.isin(coding_languages)].reset_index()
-    df_cloud_skills = skills_counts[skills_counts.index.isin(list(cloud_skills.values())+list((cloud_skills.keys())))]
+    df_cloud_skills = skills_counts[skills_counts.index.isin(list(cloud_skills.values()) + list(cloud_skills.keys()))]
 
-
-    import plotly
-    import plotly.express as px
-
-
-    fig = px.bar(df_coding_languages, x='skills', y='percent', title='Coding skills', labels={'skills': 'Skills', 'percent': 'Percentage of job applications'})
+    # Create the bar chart
+    fig = px.bar(df_coding_languages, x='skills', y='percent', title='Coding skills', labels={'index': 'Skills', 'percent': 'Percentage of job applications'})
     fig.update_yaxes(range=[0, 100])
-
 
     return fig
 
